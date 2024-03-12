e25f6c5e9495b9c155d8993578a825dbd29e9190
thabozee <thabozee@gmail.com>
2024-03-12 19:21:31 +0200

    feat:in wsgi file

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import site
 activate_this = '/var/www/html/python-applications/Maqenehelo-Chat-Bot/venv/bin/activate_this.py'
 
 with open(activate_this) as file_:
-    exec(file_.read(), dict(_file_=activate_this))
+    exec(file_.read(), dict(__file__=activate_this))
 
 import sys
 
