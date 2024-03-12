58c48efa5ea79c1c9bd051a69edc628ed156153b
thabozee <thabozee@gmail.com>
2024-03-12 18:40:55 +0200

    feat:in wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 import site
 
-activate_this = '/var/www/html/python-applications/loyalty_api/venv/bin/activate_this.py'
+activate_this = '/var/www/html/python-applications/Maqenehelo-Chat-Bot/venv/bin/activate_this.py'
 
 with open(activate_this) as file_:
     exec(file_.read(), dict(_file_=activate_this))
 
 import sys
 
-sys.path.insert(0, "/var/www/html/python-applications/loyalty_api")
+sys.path.insert(0, "/var/www/html/python-applications/Maqenehelo-Chat-Bot")
 
-site.addsitedir("/var/www/html/python-applications/loyalty_api/venv/lib64/python3.6/site-packages/")
+site.addsitedir("/var/www/html/python-applications/Maqenehelo-Chat-Bot/venv/lib64/python3.6/site-packages/")
 
 from src.view.app import build
 
